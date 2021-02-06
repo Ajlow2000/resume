6dfc2fd78edcc0e3e4a3a263756df014aeb14aeb
Ajlow2000 <ajlow2000@gmail.com>
2021-02-06 10:23:45 -0600

    Added script for new resume creation

---
+++ new_resume.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NAME="AlecLowry"
+COMPANY=$1
+POSITION=$2
+TIMESTAMP=$(date +"%Y-%m-%d")
+LIBRARY="application_library"
+SRC="cv/AlecLowry_Master-CV.tex"
+
+    # Make new directory
+new_dir_name="${TIMESTAMP}_${COMPANY}_${POSITION}"
+new_dir="${LIBRARY}/${new_dir_name}"
+mkdir -p $new_dir
+
+    # Generate default resume
+new_filename="${NAME}_${POSITION}_Resume.tex"
+cp $SRC $new_dir/$new_filename
